36a9ac95033bb0333a3fb05ba41854af0053eb81
Daniel Pecos Martinez <me@danielpecos.com>
2023-03-25 15:03:05 +0100

    vim: copilot

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,13 @@
+return {
+  "zbirenbaum/copilot.lua",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = "<tab>"
+        }
+      }
+    })
+  end,
+}
