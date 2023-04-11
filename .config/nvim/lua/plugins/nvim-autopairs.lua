b32bad0f5aa85ad939f0eff999ed570e0180f10f
Daniel Pecos Martinez <me@danielpecos.com>
2023-04-11 09:59:21 +0200

    vim: plugin not importing the correct repo

--- .config/nvim/lua/plugins/nvim-autopairs.lua
+++ .config/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,4 +1,14 @@
 return {
-  'editorconfig/editorconfig-vim',
-  event = 'VeryLazy'
+  'windwp/nvim-autopairs',
+  event = 'VeryLazy',
+  config = function()
+    require('nvim-autopairs').setup({
+      check_ts = true,
+      ts_config = {
+        lua = { 'string' },
+        javascript = { 'template_string' },
+        java = false,
+      },
+    })
+  end
 }
