49792ff853d066a7ad52041d795d4e53bd2989fe
Daniel Pecos Martinez <me@danielpecos.com>
2023-04-11 17:25:08 +0200

    vim: nvim-cmp keymaps updated

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -32,13 +32,12 @@ local setup = function()
     mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),      -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      ['<C-Space>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      -- ['<C-Space>'] = cmp.mapping.complete(),
-      ["<C-P>"] = cmp.mapping(select_next, { "i", "s" }),
-      ["<Tab>"] = cmp.mapping(select_next, { "i", "s" }),
-      ["<S-Tab>"] = cmp.mapping(select_previous, { "i", "s" }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items
+      ['<C-p>'] = cmp.mapping(select_next, { "i", "s" }),
+      -- ['<Tab>'] = cmp.mapping(select_next, { "i", "s" }),
+      -- ['<S-Tab>'] = cmp.mapping(select_previous, { "i", "s" }),
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
