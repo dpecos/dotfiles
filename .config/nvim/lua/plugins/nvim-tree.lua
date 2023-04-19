7c92813bee576cd4ed3eb5aec49b93165780b88b
Daniel Pecos Martinez <me@danielpecos.com>
2023-04-19 11:01:08 +0200

    vim: nvim-tree new mappings

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,18 +1,34 @@
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  vim.keymap.set('n', '<CR>',  api.node.open.edit,                    opts('Open'))
+  vim.keymap.set('n', 'v',     api.node.open.vertical,                opts('Open: Vertical Split'))
+  vim.keymap.set('n', 'h',     api.node.open.horizontal,              opts('Open: Horizontal Split'))
+  vim.keymap.set('n', '<Tab>', api.node.open.preview,                 opts('Open Preview'))
+
+  vim.keymap.set('n', 'a',     api.fs.create,                         opts('Create'))
+  vim.keymap.set('n', 'yy',    api.fs.copy.node,                      opts('Copy'))
+  vim.keymap.set('n', 'p',     api.fs.paste,                          opts('Paste'))
+  vim.keymap.set('n', 'd',     api.fs.remove,                         opts('Delete'))
+  vim.keymap.set('n', 'r',     api.fs.rename,                         opts('Rename'))
+
+  vim.keymap.set('n', '>',     api.node.navigate.sibling.next,        opts('Next Sibling'))
+  vim.keymap.set('n', '<',     api.node.navigate.sibling.prev,        opts('Previous Sibling'))
+  vim.keymap.set('n', 'K',     api.node.show_info_popup,              opts('Info'))
+  vim.keymap.set('n', 'H',     api.tree.toggle_hidden_filter,         opts('Toggle Dotfiles'))
+  vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+end
+
 local setup = function()
-  local nvim_tree_config = require 'nvim-tree.config'
-  local tree_cb = nvim_tree_config.nvim_tree_callback
 
   require 'nvim-tree'.setup {
     hijack_cursor = true,
     view = {
       width = 50,
-      mappings = {
-        list = {
-          { key = "u",  action = "dir_up" },
-          { key = "v",  cb = tree_cb("vsplit") },
-          { key = "yy", action = "copy" },
-        },
-      },
     },
     tab = {
       sync = {
@@ -39,6 +55,10 @@ local setup = function()
       },
     },
   }
+
+  require("nvim-tree").setup({
+    on_attach = on_attach,
+  })
 
   local api = require("nvim-tree.api")
 
