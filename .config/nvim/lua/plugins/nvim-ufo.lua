d735afec40891164035a31be03cbf35f2fbfef65
Daniel Pecos Martinez <me@danielpecos.com>
2023-04-21 16:53:15 +0200

    vim: folds re-enabled

--- .config/nvim/lua/plugins/nvim-ufo.lua
+++ .config/nvim/lua/plugins/nvim-ufo.lua
@@ -6,8 +6,6 @@ return {
   },
   event = 'VeryLazy',
   config = function()
-    require('ufo').setup()
-
     vim.o.foldcolumn = '0' -- '0' is not bad
     vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
     vim.o.foldlevelstart = 99
@@ -16,6 +14,7 @@ return {
     -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
     vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
     vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+    require('ufo').setup()
   end,
-  enabled = false
 }
