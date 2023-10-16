b4ccc64ec59bb6511450eab85a437bbb27595f4a
Daniel Pecos Martinez <me@danielpecos.com>
2023-10-16 10:07:38 +0200

    vi: yamlfmt formatter settings

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -20,14 +20,13 @@ return {
     formatters_by_ft = formatters,
     format_on_save = { timeout_ms = 500, lsp_fallback = true },
   },
-  -- init = function()
+  --init = function()
   --   -- If you want the formatexpr, here is the place to set it
   --   vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
   -- end,
-  -- -- This function is optional, but if you want to customize formatters do it here
-  -- config = function(_, opts)
-  --   local util = require("conform.util")
-  --   util.add_formatter_args(require("conform.formatters.shfmt"), { "-i", "2" })
-  --   require("conform").setup(opts)
-  -- end,
+  config = function(_, opts)
+    local util = require("conform.util")
+    util.add_formatter_args(require("conform.formatters.yamlfmt"), { "-formatter", "retain_line_breaks=true" })
+    require("conform").setup(opts)
+  end,
 }
