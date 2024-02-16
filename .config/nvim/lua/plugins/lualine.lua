6852502bd5ebf85aa8a3f297e0fb6bc5f94440f8
Daniel Pecos Martinez <me@danielpecos.com>
2024-02-16 13:45:34 +0100

    vim: add indicater in status line about copilot

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,38 +1,39 @@
 return {
-  'nvim-lualine/lualine.nvim',
+  "nvim-lualine/lualine.nvim",
   dependencies = {
     --'kyazdani42/nvim-web-devicons'
-    'Mofiqul/vscode.nvim'
+    "Mofiqul/vscode.nvim",
+    "AndreM222/copilot-lualine",
   },
   config = function()
-    require('lualine').setup({
+    require("lualine").setup({
       options = {
         icons_enabled = true,
         -- theme = 'vscode',
-        theme = 'onedark',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
+        theme = "onedark",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
         disabled_filetypes = {},
         always_divide_middle = true,
       },
       sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "copilot", "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
       },
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
         lualine_y = {},
-        lualine_z = {}
+        lualine_z = {},
       },
       tabline = {},
-      extensions = { 'nvim-tree' }
+      extensions = { "nvim-tree" },
     })
-  end
+  end,
 }
