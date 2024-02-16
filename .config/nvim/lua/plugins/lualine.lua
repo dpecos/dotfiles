f0fae14b367dd74286a48cefb77405d236eddafe
Daniel Pecos Martinez <me@danielpecos.com>
2024-02-16 20:21:38 +0100

    vim: copilot icon with colors

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,39 +1,48 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = {
-    --'kyazdani42/nvim-web-devicons'
-    "Mofiqul/vscode.nvim",
-    "AndreM222/copilot-lualine",
-  },
-  config = function()
-    require("lualine").setup({
-      options = {
-        icons_enabled = true,
-        -- theme = 'vscode',
-        theme = "onedark",
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-        disabled_filetypes = {},
-        always_divide_middle = true,
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { "filename" },
-        lualine_x = { "copilot", "encoding", "fileformat", "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
-      },
-      tabline = {},
-      extensions = { "nvim-tree" },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		--'kyazdani42/nvim-web-devicons'
+		"Mofiqul/vscode.nvim",
+		"AndreM222/copilot-lualine",
+	},
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				-- theme = 'vscode',
+				theme = "onedark",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {},
+				always_divide_middle = true,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = {
+					{
+						"copilot",
+						show_colors = true,
+						show_loading = true,
+					},
+					"encoding",
+					"fileformat",
+					"filetype",
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			extensions = { "nvim-tree" },
+		})
+	end,
 }
