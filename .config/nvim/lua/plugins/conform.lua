70bdde258505a34de7f140af06df1f6d11bba45f
Daniel Pecos Martinez <me@danielpecos.com>
2025-06-23 16:51:26 +0200

    vim: removed shorcodes from shortcuts descriptions

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,67 +1,67 @@
 local formatters = require("plugins/local/mason-tools").formatters
 
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  keys = {
-    {
-      -- Customize or remove this keymap to your liking
-      "<leader>f",
-      function()
-        require("conform").format({ async = true, lsp_fallback = true })
-      end,
-      mode = "",
-      desc = "Conform: [f]ormat buffer",
-    },
-  },
-  -- Everything in opts will be passed to setup()
-  opts = {
-    formatters_by_ft = formatters,
-    format_on_save = function(bufnr)
-      -- Disable with a global or buffer-local variable
-      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-        return
-      end
-      -- Disable autoformat for files in a certain path
-      local bufname = vim.api.nvim_buf_get_name(bufnr)
-      if bufname:match("/node_modules/") then
-        return
-      end
-      return { timeout_ms = 500, lsp_fallback = true }
-    end,
-    formatters = {
-      rustfmt = {
-        command = "rustfmt",
-        args = { "--edition=2021", "--emit=stdout" },
-      },
-    },
-  },
-  --init = function()
-  --   -- If you want the formatexpr, here is the place to set it
-  --   vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  -- end,
-  config = function(_, opts)
-    local util = require("conform.util")
-    util.add_formatter_args(require("conform.formatters.yamlfmt"), { "-formatter", "retain_line_breaks=true" })
-    require("conform").setup(opts)
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Conform: format buffer",
+		},
+	},
+	-- Everything in opts will be passed to setup()
+	opts = {
+		formatters_by_ft = formatters,
+		format_on_save = function(bufnr)
+			-- Disable with a global or buffer-local variable
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+			-- Disable autoformat for files in a certain path
+			local bufname = vim.api.nvim_buf_get_name(bufnr)
+			if bufname:match("/node_modules/") then
+				return
+			end
+			return { timeout_ms = 500, lsp_fallback = true }
+		end,
+		formatters = {
+			rustfmt = {
+				command = "rustfmt",
+				args = { "--edition=2021", "--emit=stdout" },
+			},
+		},
+	},
+	--init = function()
+	--   -- If you want the formatexpr, here is the place to set it
+	--   vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	-- end,
+	config = function(_, opts)
+		local util = require("conform.util")
+		util.add_formatter_args(require("conform.formatters.yamlfmt"), { "-formatter", "retain_line_breaks=true" })
+		require("conform").setup(opts)
 
-    vim.api.nvim_create_user_command("FormatDisable", function(args)
-      if args.bang then
-        -- FormatDisable! will disable formatting just for this buffer
-        vim.b.disable_autoformat = true
-      else
-        vim.g.disable_autoformat = true
-      end
-    end, {
-      desc = "Disable autoformat-on-save",
-      bang = true,
-    })
-    vim.api.nvim_create_user_command("FormatEnable", function()
-      vim.b.disable_autoformat = false
-      vim.g.disable_autoformat = false
-    end, {
-      desc = "Re-enable autoformat-on-save",
-    })
-  end,
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
+	end,
 }
