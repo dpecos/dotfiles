f90a94943296cf7ad6b6065f7cfdcb4ce60173cd
Daniel Pecos Martinez <me@danielpecos.com>
2025-10-15 13:38:04 +0200

    vi: another fix for non-colored icons

--- .config/nvim/lua/plugins/vscode.lua
+++ .config/nvim/lua/plugins/vscode.lua
@@ -6,26 +6,26 @@
 -- Familiar color palette for developers coming from VSCode
 
 return {
-	"Mofiqul/vscode.nvim",
-	event = "VeryLazy",
-	config = function()
-		require("vscode").setup({
-			style = "dark",
+  "Mofiqul/vscode.nvim",
+  priority = 1000,
+  config = function()
+    require("vscode").setup({
+      style = "dark",
 
-			transparent = true,
+      transparent = true,
 
-			-- Enable italic comment
-			italic_comments = true,
+      -- Enable italic comment
+      italic_comments = true,
 
-			-- Disable nvim-tree background color
-			disable_nvimtree_bg = false,
+      -- Disable nvim-tree background color
+      disable_nvimtree_bg = false,
 
-			-- Override colors (see ./lua/vscode/colors.lua)
-			-- color_overrides = {
-			--   vscLineNumber = '#FF0000',
-			-- },
-		})
+      -- Override colors (see ./lua/vscode/colors.lua)
+      -- color_overrides = {
+      --   vscLineNumber = '#FF0000',
+      -- },
+    })
 
-		require("vscode").load()
-	end,
+    require("vscode").load()
+  end,
 }
