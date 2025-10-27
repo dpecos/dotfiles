aad7f6aec94ddb69b68414638ee13af9784cbe9c
Daniel Pecos Martinez <me@danielpecos.com>
2025-10-27 16:22:37 +0100

    vi: spectre keymap descriptions prefixed

--- .config/nvim/lua/plugins/nvim-spectre.lua
+++ .config/nvim/lua/plugins/nvim-spectre.lua
@@ -10,9 +10,9 @@ return {
     'nvim-lua/plenary.nvim'
   },
   keys = {
-    {'<leader>S', '<cmd>lua require("spectre").open()<CR>', desc = "Open Spectre" },
-    {'<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', desc = "Search current word" },
-    {'<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', desc = "Search current word", mode = 'v'},
-    {'<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', desc = "Search on current file" }
+    { '<leader>S',  '<cmd>lua require("spectre").open()<CR>',                               desc = "Spectre: Open Spectre" },
+    { '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>',      desc = "Spectre: Search current word" },
+    { '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>',                   desc = "Spectre: Search current word",   mode = 'v' },
+    { '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', desc = "Spectre: Search on current file" }
   }
 }
