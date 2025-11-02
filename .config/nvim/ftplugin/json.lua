b94df5b6afd579aa533af6cd500382c8116968d6
Daniel Pecos Martinez <me@danielpecos.com>
2025-11-02 10:07:57 +0100

    vi: json mappings updated

--- .config/nvim/ftplugin/json.lua
+++ .config/nvim/ftplugin/json.lua
@@ -1,10 +1,17 @@
-vim.keymap.set('n', 'o', function()
-  local line = vim.api.nvim_get_current_line()
+local map = require("utils.keymap").map
 
-  local should_add_comma = string.find(line, '[^,{[]$')
-  if should_add_comma then
-    return 'A,<cr>'
-  else
-    return 'o'
-  end
-end, { buffer = true, expr = true })
+map("o",
+  function()
+    local line = vim.api.nvim_get_current_line()
+
+    local should_add_comma = string.find(line, '[^,{[]$')
+    if should_add_comma then
+      return 'A,<cr>'
+    else
+      return 'o'
+    end
+  end,
+  'JSON',
+  'Add comma on new line if needed',
+  { buffer = true, expr = true }
+)
