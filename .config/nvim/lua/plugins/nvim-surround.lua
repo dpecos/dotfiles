f8dfd40d2dfc19fec5632bd76e5689c2ccb5df47
Daniel Pecos Martinez <me@danielpecos.com>
2026-03-28 20:20:51 +0100

    vim: nvim-surround with default config

--- .config/nvim/lua/plugins/nvim-surround.lua
+++ .config/nvim/lua/plugins/nvim-surround.lua
@@ -6,55 +6,7 @@
 -- Examples: ysiw", ds", cs"', yss)
 
 return {
-	"kylechui/nvim-surround",
-	version = "*",
-	event = "VeryLazy",
-	config = function()
-		require("nvim-surround").setup({
-			-- Default keymaps:
-			-- ys{motion}{char} - Add surround
-			-- ds{char} - Delete surround
-			-- cs{target}{replacement} - Change surround
-			keymaps = {
-				insert = "<C-g>s",
-				insert_line = "<C-g>S",
-				normal = "ys",
-				normal_cur = "yss",
-				normal_line = "yS",
-				normal_cur_line = "ySS",
-				visual = "S",
-				visual_line = "gS",
-				delete = "ds",
-				change = "cs",
-				change_line = "cS",
-			},
-			-- Custom surroundings can be added here
-			surrounds = {
-				-- Example: Add custom markdown link surround
-				-- ["l"] = {
-				-- 	add = function()
-				-- 		local clipboard = vim.fn.getreg("+"):gsub("\n", "")
-				-- 		return {
-				-- 			{ "[" },
-				-- 			{ "](" .. clipboard .. ")" },
-				-- 		}
-				-- 	end,
-				-- },
-			},
-			-- Enable/disable default surrounds
-			-- e.g., to disable ( and ) surrounds: ["("] = false, [")"] = false
-			aliases = {
-				["a"] = ">", -- HTML tag
-				["b"] = ")", -- Parentheses
-				["B"] = "}", -- Braces
-				["r"] = "]", -- Brackets
-				["q"] = { '"', "'", "`" }, -- Quotes
-				["s"] = { "}", "]", ")", ">", '"', "'", "`" }, -- All surrounding pairs
-			},
-			highlight = {
-				duration = 0,
-			},
-			move_cursor = "begin",
-		})
-	end,
+  "kylechui/nvim-surround",
+  version = "*",
+  event = "VeryLazy",
 }
