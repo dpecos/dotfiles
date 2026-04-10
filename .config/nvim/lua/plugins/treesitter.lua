8d923acdb567b67f52608f0f784291c09228bf8e
Daniel Pecos Martinez <me@danielpecos.com>
2026-04-10 10:25:38 +0200

    vim: treesitter setup handled manually

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,47 +1,75 @@
 vim.pack.add({
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter",              version = "main" },
-  { src = "https://github.com/MeanderingProgrammer/treesitter-modules.nvim", version = "main" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 })
 
-local languages = {
-  'bash',
-  'comment',
-  'css',
-  'diff',
-  'fish',
+-- Languages to ensure are installed
+local ensure_installed = {
+  -- Systems programming
+  "c",
+  "cpp",
+  "rust",
+  "go",
+
+  -- Scripting
+  "lua",
+  "python",
+  "bash",
+  "javascript",
+  "typescript",
+  "query",
+  "regex",
+
+  -- Web development
+  "html",
+  "css",
+  "vue",
+  "svelte",
+  "markdown",
+  "markdown_inline",
+  "json",
+  "yaml",
+  "toml",
+
+  -- Data
+  "sql",
+  "xml",
+  "csv",
+
+  -- Build systems
+  "dockerfile",
+  "make",
+  "cmake",
+
+  -- Others
+  "vim",
+  "query",
+  "regex",
+  "comment",
   'git_config',
   'git_rebase',
   'gitcommit',
   'gitignore',
-  'html',
-  'javascript',
-  'json',
-  'latex',
-  'lua',
-  'luadoc',
-  'make',
   'markdown',
   'markdown_inline',
-  'python',
-  'query',
-  'regex',
-  'scss',
-  'svelte',
-  'toml',
-  'tsx',
-  'typescript',
-  'typst',
-  'vim',
-  'vimdoc',
-  'vue',
-  'xml',
 }
 
-require("treesitter-modules").setup({
-  ensure_installed = languages,
-  auto_install = false,
-  fold = { enable = true },
-  highlight = { enable = true },
-  indent = { enable = true },
-  incremental_selection = { enable = true },
+-- Enable treesitter for all filetypes that have a parser available
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '<filetype>' },
+  callback = function()
+    vim.treesitter.start()
+  end,
 })
+
+-- Setup function - call this to initialize treesitter
+local function setup()
+  local ts_ok, ts = pcall(require, "nvim-treesitter")
+  if not ts_ok then
+    vim.notify("Failed to load nvim-treesitter: " .. ts, vim.log.levels.ERROR, { title = "Treesitter" })
+  else
+    ts.install(ensure_installed)
+    ts.update()
+  end
+end
+
+setup()
